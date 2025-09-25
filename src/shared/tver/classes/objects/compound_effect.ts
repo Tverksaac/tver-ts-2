84c3043969be2d7f4b4ae0f5d615fc40b81b5a7e
Tverksaac <artemosha12@gmail.com>
2025-09-25 20:45:50 +0300

    commit

--- src/shared/tver/classes/objects/compound_effect.ts
+++ src/shared/tver/classes/objects/compound_effect.ts
@@ -1,9 +1,5 @@
-import { PropertyNames } from "shared/tver/utility/_ts_only/types"
 import { CustomPropertyEffect, StrictPropertyEffect } from "../fundamental/property_effect"
 import {CustomStatEffect, StrictStatEffect } from "../fundamental/stat_effect"
-import { JumpHeightEffect, WalkSpeedEffect } from "shared/tver/test/stat_effect_classes";
-import { AutoRotateEffect } from "shared/tver/test/property_effect_classes";
-
 
 export abstract class CompoundEffect {
     public abstract readonly duration: number;
@@ -12,24 +8,4 @@ export abstract class CompoundEffect {
     public abstract readonly PropertyEffects: (StrictPropertyEffect<never, never> | CustomPropertyEffect)[]
 
     constructor () {}
-}
-
-export class Stun extends CompoundEffect {
-    public readonly duration: number;
-
-    public readonly PropertyEffects: (StrictPropertyEffect<never, never> | CustomPropertyEffect)[];
-    public readonly StatEffects: (StrictStatEffect<never> | CustomStatEffect)[];
-
-    constructor (_duration: number) {
-        super()
-        this.duration = _duration
-
-        this.PropertyEffects = [
-            new AutoRotateEffect(false, this.duration)
-        ]
-        this.StatEffects = [
-            new WalkSpeedEffect("Modifer", 0, this.duration),
-            new JumpHeightEffect("Modifer", 0, this.duration)
-        ]
-    }
 }
---
+++ src/shared/tver/test/compound_effects_classes.ts
@@ -0,0 +1,25 @@
+import { CustomPropertyEffect, StrictPropertyEffect } from "../classes/fundamental/property_effect";
+import { CustomStatEffect, StrictStatEffect } from "../classes/fundamental/stat_effect";
+import { CompoundEffect } from "../classes/objects/compound_effect";
+import { AutoRotateEffect } from "./property_effect_classes";
+import { JumpHeightEffect, WalkSpeedEffect } from "./stat_effect_classes";
+
+export class Stun extends CompoundEffect {
+    public readonly duration: number;
+
+    public readonly PropertyEffects: (StrictPropertyEffect<never, never> | CustomPropertyEffect)[];
+    public readonly StatEffects: (StrictStatEffect<never> | CustomStatEffect)[];
+
+    constructor (_duration: number) {
+        super()
+        this.duration = _duration
+
+        this.PropertyEffects = [
+            new AutoRotateEffect(false, this.duration)
+        ]
+        this.StatEffects = [
+            new WalkSpeedEffect("Modifer", 0, this.duration),
+            new JumpHeightEffect("Modifer", 0, this.duration)
+        ]
+    }
+}
